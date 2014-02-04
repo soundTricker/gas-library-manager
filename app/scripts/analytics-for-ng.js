4c995b386f3398f23e74d98b7fe33a7bcab48466
keisuke oohashi <keisuke.oohashi@gmail.com>
2014-02-04 15:48:09 +0900

    create un-autotrac-analytics.js

---
+++ app/scripts/analytics-for-ng.js
@@ -0,0 +1,18 @@
+(function() {
+  var _gaq;
+
+  _gaq = _gaq || [];
+
+  _gaq.push(["_setAccount", "UA-44064442-1"]);
+
+  (function() {
+    var ga, s;
+    ga = document.createElement("script");
+    ga.type = "text/javascript";
+    ga.async = true;
+    ga.src = "https://ssl.google-analytics.com/ga.js";
+    s = document.getElementsByTagName("script")[0];
+    return s.parentNode.insertBefore(ga, s);
+  })();
+
+}).call(this);
